cc604711d31ce944ff14221d31b99f5de8a605f9
Abhishek Dhar <abhishekdhar1603@gmail.com>
2021-10-03 23:57:15 +0530

    minor imp changes

--- Java_sde.java
+++ Java_sde.java
@@ -15,21 +15,21 @@ public class Java_sde {
     /**
      * @param args the command line arguments
      */
-//    public static void main(String[] args) {
-//        
-//        int n1=0,n2=1,n3,i,count=10;
-//        
-//        System.out.println(n1+"\n"+n2);
-//        
-//        for(i=0;i<count;i++)
-//        {
-//            n3 =  n1+n2;
-//            System.out.println(n3);
-//            
-//            n1 = n2;
-//            n2 = n3;
-//        }
-//    }
+   public static void main(String[] args) {
+       
+       int n1=0,n2=1,n3,i,count=10;
+       
+       System.out.println(n1+"\n"+n2);
+       
+       for(i=0;i<count;i++)
+       {
+           n3 =  n1+n2;
+           System.out.println(n3);
+           
+           n1 = n2;
+           n2 = n3;
+       }
+   }
     static int n1=0, n2=1, n3=0;
     static void printFibonacci(int count)
     {
